5bfc71a5941a5bf3c4bc230f4bdb321d22f7a9dc
Renato Massaro <renato@hackerexperience.com>
2016-02-07 01:57:10 -0200

    Better setup information for PyPi

--- setup.py
+++ setup.py
@@ -6,12 +6,14 @@ print(find_packages())
 setup(
     name='HEpigram',
     version='0.1',
-    url='asdf',
-    author='asdf',
-    author_email='asdf',
-    description=('asdf'),
+    url='https://dev.hackerexperience.com/diffusion/HEPG/',
+    author='Renato Massaro',
+    author_email='renato@hackerexperience.com',
+    description=('HEpigram provides a friendly interface for documents that '
+                 'need to be generated/compiled by tools like MkDocs.'),
     license='MIT',
     packages=find_packages(),
+    keywords=['documentation'],
     entry_points='''
         [console_scripts]
         hepigram=hepigram.app:cli
